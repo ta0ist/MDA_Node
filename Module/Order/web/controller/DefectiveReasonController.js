5ae8f561ba343203ce90f9da56419269c70cf679
hjjisgood <814287235@qq.com>
2016-12-30 16:42:22 +0800

    commit

--- Module/Order/web/controller/DefectiveReasonController.js
+++ Module/Order/web/controller/DefectiveReasonController.js
@@ -2,7 +2,7 @@
  * Created by qb on 2016/12/21.
  */
 var baseUrl = '/DefectiveReason/';
-$(function () {
+$(function() {
     var fields = {
         INFERIOR_NO: { type: "string" },
         INFERIOR_NBR: { type: "string" },
@@ -10,16 +10,17 @@ $(function () {
         MEMO: { type: "string" }
     };
     var cols = [];
-    cols.push({ field: "INFERIOR_NBR", title: "原因编号", width: 80, sortable: true, filterable: false, hidden: true });
-    cols.push({ field: "INFERIOR_NO", title: "原因编号", width: 80, sortable: true, filterable: false, hidden: false });
-    cols.push({ field: "INFERIOR_NAME", title: "原因名称", width: 80, sortable: true, filterable: false });
-    cols.push({ field: "MEMO", title: "备注", width: 80, sortable: true, filterable: { extra: false } });
+    cols.push({ field: "INFERIOR_NBR", title: lang.Order.ReasonNumber, width: 80, sortable: true, filterable: false, hidden: true });
+    cols.push({ field: "INFERIOR_NO", title: lang.Order.ReasonNumber, width: 80, sortable: true, filterable: false, hidden: false });
+    cols.push({ field: "INFERIOR_NAME", title: lang.Order.BecauseName, width: 80, sortable: true, filterable: false });
+    cols.push({ field: "MEMO", title: lang.Order.Note, width: 80, sortable: true, filterable: { extra: false } });
     cols.push({
         command: [
-            { name: "aa", text: '编辑' + '<i class="icon-edit"></i>', className: "btn purple", click: f_edit },
-            { name: "bb", text: '删除' + '<i class="icon-remove-sign"></i>', className: "btn red ", click: f_delete }
+            { name: "aa", text: lang.Order.Edit + '<i class="icon-edit"></i>', className: "btn purple", click: f_edit },
+            { name: "bb", text: lang.Order.Delete + '<i class="icon-remove-sign"></i>', className: "btn red ", click: f_delete }
         ],
-        title: '操作', width: 200
+        title: lang.Order.Operation,
+        width: 200
     });
 
     grid = $("#grid").grid({
@@ -44,25 +45,23 @@ $(function () {
         }
     });
 
-    $("#error_a").click(function (data) {
-        $.x5window("新增", kendo.template($("#add").html()));
-        $("#Save").click(function () {
+    $("#error_a").click(function(data) {
+        $.x5window(lang.Order.Add, kendo.template($("#add").html()));
+        $("#Save").click(function() {
             var INFERIOR_NO = $("#INFERIOR_NO").val();
             var INFERIOR_NAME = $("#INFERIOR_NAME").val();
-            if (INFERIOR_NO == "" || INFERIOR_NAME == "")
-            { BzAlert("基础数据没有填全！"); return }
-            var inf={
-                INFERIOR_NO:INFERIOR_NO,
-                INFERIOR_NAME:INFERIOR_NAME,
-                MEMO:$("#MEMO").val()
+            if (INFERIOR_NO == "" || INFERIOR_NAME == "") { BzAlert(lang.Order.PromptInformation); return }
+            var inf = {
+                INFERIOR_NO: INFERIOR_NO,
+                INFERIOR_NAME: INFERIOR_NAME,
+                MEMO: $("#MEMO").val()
             }
-            $.post(baseUrl + "addReason", inf, function (data) {
+            $.post(baseUrl + "addReason", inf, function(data) {
                 if (data.Status == 0) {
                     $("#x5window").data("kendoWindow").close();
                     BzSuccess(data.Message);
                     grid.grid("refresh", []);
-                }
-                else {
+                } else {
                     BzAlert(data.Message);
                 }
             })
@@ -70,29 +69,27 @@ $(function () {
     })
 
     function f_edit(e) {
-        $.x5window("编辑", kendo.template($("#add").html()));
+        $.x5window(lang.Order.Edit, kendo.template($("#add").html()));
         dataItem = this.dataItem($(e.currentTarget).closest("tr"));
         $("#INFERIOR_NO").val(dataItem.INFERIOR_NO);
         $("#INFERIOR_NAME").val(dataItem.INFERIOR_NAME);
         $("#MEMO").val(dataItem.MEMO);
-        $("#Save").click(function () {
+        $("#Save").click(function() {
             var INFERIOR_NO = $("#INFERIOR_NO").val();
             var INFERIOR_NAME = $("#INFERIOR_NAME").val();
-            if (INFERIOR_NO == "" || INFERIOR_NAME == "")
-            { BzAlert("基础数据没有填全！"); return }
+            if (INFERIOR_NO == "" || INFERIOR_NAME == "") { BzAlert(lang.Order.PromptInformation); return }
             var inf = {
-                INFERIOR_NBR:dataItem.INFERIOR_NBR,
+                INFERIOR_NBR: dataItem.INFERIOR_NBR,
                 INFERIOR_NO: INFERIOR_NO,
                 INFERIOR_NAME: INFERIOR_NAME,
                 MEMO: $("#MEMO").val()
             }
-            $.post(baseUrl + "modifyReason", inf, function (data) {
+            $.post(baseUrl + "modifyReason", inf, function(data) {
                 if (data.Status == 0) {
                     $("#x5window").data("kendoWindow").close();
                     BzSuccess(data.Message);
                     grid.grid("refresh", []);
-                }
-                else {
+                } else {
                     BzAlert(data.Message);
                 }
             })
@@ -103,13 +100,12 @@ $(function () {
 
     function f_delete(e) {
         dataItem = this.dataItem($(e.currentTarget).closest("tr"));
-        var data=dataItem.INFERIOR_NBR;
-        $.post(baseUrl + "deleteReason", {inferior_nbr:data}, function (data) {
+        var data = dataItem.INFERIOR_NBR;
+        $.post(baseUrl + "deleteReason", { inferior_nbr: data }, function(data) {
             if (data.Status == 0) {
                 BzSuccess(data.Message);
                 grid.grid("refresh", [])
-            }
-            else {
+            } else {
                 BzAlert(data.Message);
             }
         })
