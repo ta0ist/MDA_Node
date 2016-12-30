fc584ce6ddd476eee622968ed23b7066aed7909a
hjjisgood <814287235@qq.com>
2016-12-30 16:31:41 +0800

    commit

--- Module/Order/node/controller/Defective.js
+++ Module/Order/node/controller/Defective.js
@@ -1,58 +1,67 @@
 /**
  * Created by qb on 2016/12/21.
  */
-var path=require('path');
-var config=require('../../../../routes/config');
+var path = require('path');
+var config = require('../../../../routes/config');
 var post_argu = require('../../../../routes/post_argu.js');
 var session = require('express-session');
-exports.Defective=function(req,res){
+exports.Defective = function(req, res) {
     if (!req.session.user)
         res.redirect('/');
     else {
-        res.render(path.resolve(__dirname, '../../web/view/Defective/index'), { menulist: req.session.menu,user:req.session.user });
+        res.render(path.resolve(__dirname, '../../web/view/Defective/index'), {
+            menulist: req.session.menu,
+            user: req.session.user,
+            lang: post_argu.getLanguage()
+        });
     }
 }
-exports.fun=function(req,res){
-    var args=[];
+exports.fun = function(req, res) {
+    var args = [];
     args.push(res);
     //var method = config.webMachineWorkingStateService + '/'+ req.params.method;
-    var method = post_argu.getpath(__filename,req.params.method);
+    var method = post_argu.getpath(__filename, req.params.method);
     args.push(method);
     args.push(req.body);
     args.push(req);
-    doCallback(eval(req.params.method),args,res,req);
+    doCallback(eval(req.params.method), args, res, req);
 }
-function doCallback(fn,args,res) {
+
+function doCallback(fn, args, res) {
     fn.apply(this, args);
 }
-function getPlanSchedule(res,method,args){
 
-    post_argu.post_argu(res,method,{PageModel:args});
+function getPlanSchedule(res, method, args) {
+
+    post_argu.post_argu(res, method, { PageModel: args });
 }
-function GetDefectiveRateByDate(res,method,args){
 
-    var data={
-        startDate:args.startDate,
-        endDate:args.endDate,
-        Prod_nbrList:convenient_tool(args['Prod_nbrList[]'])
+function GetDefectiveRateByDate(res, method, args) {
+
+    var data = {
+        startDate: args.startDate,
+        endDate: args.endDate,
+        Prod_nbrList: convenient_tool(args['Prod_nbrList[]'])
     }
-    post_argu.post_argu(res,method,data);
+    post_argu.post_argu(res, method, data);
 }
-function GetProductList(res,method,args){
 
-    post_argu.post_argu(res,method,{ProNo:0});
-}
-function GetProductListByKey(res,method,args){
+function GetProductList(res, method, args) {
 
-    post_argu.post_argu(res,method,args);
+    post_argu.post_argu(res, method, { ProNo: 0 });
 }
-function convenient_tool(arr){
-    var re=[];
-    if((typeof arr)=='string'){
+
+function GetProductListByKey(res, method, args) {
+
+    post_argu.post_argu(res, method, args);
+}
+
+function convenient_tool(arr) {
+    var re = [];
+    if ((typeof arr) == 'string') {
         re.push(arr);
-    }else{
-        re=arr;
+    } else {
+        re = arr;
     }
     return re;
 }
-
--- Module/Order/web/controller/DefectiveController.js
+++ Module/Order/web/controller/DefectiveController.js
@@ -3,7 +3,7 @@
  */
 var baseUrl = "/Defective/";
 var grid;
-$(function () {
+$(function() {
     $("#startTime").kendoDatePicker({ format: "yyyy/MM/dd", value: new Date() });
     $("#endTime").kendoDatePicker({ format: "yyyy/MM/dd", value: new Date() });
     PROD_NO = $("#PROD_NO").multipleComboxTree({
@@ -16,8 +16,7 @@ $(function () {
         },
         inputheight: 34,
         checkbox: false,
-        select: function (data) {
-        }
+        select: function(data) {}
     }).data("BZ-multipleComboxTree");
 
 
@@ -34,7 +33,7 @@ $(function () {
         CRAFT_NAME: { type: "string" },
         PROC_NO: { type: "string" },
         PROC_NAME: { type: "string" },
-        INFERIOR_NO:{type:"string"},
+        INFERIOR_NO: { type: "string" },
         INFERIOR_NAME: { type: "string" },
         INPUT_NUM: { type: "string" },
         TARGET_NUM: { type: "string" },
@@ -47,31 +46,31 @@ $(function () {
         INFERIOR_NUM_RATE: { type: "string" },
         MEN_NO: { type: "string" },
         MAN_NAMES: { type: "string" },
-        REPORT_DATE: {type:"date"}
+        REPORT_DATE: { type: "date" }
     }
 
     var cols = [];
-    cols.push({ field: "PROD_NO", title: "产品编号", width: 80, sortable: true, filterable: false, hidden: false });
-    cols.push({ field: "PROD_NAME", title: "产品名称", width: 80, sortable: true, filterable: false, hidden: false });
-    cols.push({ field: "PLAN_NO", title: "生产计划", width: 80, sortable: true, filterable: false, hidden: false });
-    cols.push({ field: "TASK_NO", title: "工单号", width: 80, sortable: true, filterable: false, hidden: false });
-    cols.push({ field: "MAC_NO", title: "设备编号", width: 80, sortable: true, filterable: false, hidden: false });
-    cols.push({ field: "MAC_NAME", title: "设备名称", width: 80, sortable: true, filterable: false, hidden: false });
+    cols.push({ field: "PROD_NO", title: lang.Order.ProductNumber, width: 80, sortable: true, filterable: false, hidden: false });
+    cols.push({ field: "PROD_NAME", title: lang.Order.ProductName, width: 80, sortable: true, filterable: false, hidden: false });
+    cols.push({ field: "PLAN_NO", title: lang.Order.ProductionPlan, width: 80, sortable: true, filterable: false, hidden: false });
+    cols.push({ field: "TASK_NO", title: lang.Order.WorkOrderNo, width: 80, sortable: true, filterable: false, hidden: false });
+    cols.push({ field: "MAC_NO", title: lang.Order.EquipmentSerialNumber, width: 80, sortable: true, filterable: false, hidden: false });
+    cols.push({ field: "MAC_NAME", title: lang.Order.DeviceName, width: 80, sortable: true, filterable: false, hidden: false });
 
-    cols.push({ field: "REPORT_DATE", title: "派工日期", width: 80, sortable: true, filterable: true, hidden: false, format: "{0: yyyy/MM/dd HH:mm:ss}" });
-    cols.push({ field: "END_DATE", title: "计划结束日期", width: 80, sortable: true, filterable: false, hidden: true, format: "{0: yyyy/MM/dd HH:mm:ss}" });
+    cols.push({ field: "REPORT_DATE", title: lang.Order.DispatchDate, width: 80, sortable: true, filterable: true, hidden: false, format: "{0: yyyy/MM/dd HH:mm:ss}" });
+    cols.push({ field: "END_DATE", title: lang.Order.PlansEndDate, width: 80, sortable: true, filterable: false, hidden: true, format: "{0: yyyy/MM/dd HH:mm:ss}" });
     cols.push({ field: "PROD_NBR", title: "产品nbr", width: 80, sortable: true, filterable: false, hidden: true });
     //cols.push({ field: "CRAFT_NO", title: "工序编号", width: 80, sortable: true, filterable: false, hidden: false });
     //cols.push({ field: "CRAFT_NAME", title: "工序名称", width: 80, sortable: true, filterable: false, hidden: false });
-    cols.push({ field: "INFERIOR_NO", title: "原因编号", width: 80, sortable: true, filterable: false, hidden: false });
-    cols.push({ field: "INFERIOR_NAME", title: "原因名称", width: 80, sortable: true, filterable: false, hidden: false });
+    cols.push({ field: "INFERIOR_NO", title: lang.Order.ReasonNumber, width: 80, sortable: true, filterable: false, hidden: false });
+    cols.push({ field: "INFERIOR_NAME", title: lang.Order.BecauseName, width: 80, sortable: true, filterable: false, hidden: false });
     //cols.push({ field: "TARGET_NUM", title: "目标产量", width: 80, sortable: true, filterable: false, hidden: false });
     //cols.push({ field: "QUALIFIED_NUM", title: "正品品数", width: 80, sortable: true, filterable: false, hidden: false });
-    cols.push({ field: "INFERIOR_NUM", title: "次品数", width: 80, sortable: true, filterable: false, hidden: false });
+    cols.push({ field: "INFERIOR_NUM", title: lang.Order.DefectiveGoodsNumber, width: 80, sortable: true, filterable: false, hidden: false });
     //cols.push({ field: "QUALITY_RATE", title: "质量率", width: 80, sortable: true, filterable: false, hidden: false, template: kendo.template($("#QUALITY_RATE").html()) });
     //cols.push({ field: "UNIT", title: "单位", width: 80, sortable: true, filterable: false, hidden: false });
     cols.push({ field: "MEN_NO", title: "操作人员", width: 80, sortable: true, filterable: false, hidden: true });
-    cols.push({ field: "MAN_NAMES", title: "操作人员", width: 120, sortable: true, filterable: false, hidden: false });
+    cols.push({ field: "MAN_NAMES", title: lang.Order.Operator, width: 120, sortable: true, filterable: false, hidden: false });
 
     grid = $("#grid").grid({
         //checkBoxColumn: true,
@@ -97,12 +96,12 @@ $(function () {
 
     });
 
-    $("#search").click(function (data) {
+    $("#search").click(function(data) {
         var startDate = $("#startTime").val();
         var endDate = $("#endTime").val();
 
 
-        grid.grid("refresh", function () {
+        grid.grid("refresh", function() {
             return [
                 { field: "PROD_NBR", Operator: "eq", value: PROD_NO.rData },
                 { field: "REPORT_DATE", Operator: "gte", value: startDate },
@@ -119,7 +118,7 @@ $(function () {
 function highchart(startDate, endDate) {
     var Prod_nbrList = [];
     Prod_nbrList.push(PROD_NO.rData);
-    $.post(baseUrl + "GetDefectiveRateByDate", { startDate: startDate, endDate: endDate, Prod_nbrList: Prod_nbrList }, function (data) {
+    $.post(baseUrl + "GetDefectiveRateByDate", { startDate: startDate, endDate: endDate, Prod_nbrList: Prod_nbrList }, function(data) {
         if (data.Status == 0) {
             gridr(data.Data.ReasonList, 1);
             gridr(data.Data.MacList, 2);
@@ -133,9 +132,7 @@ function gridr(data, type) {
     var categories = [];
     var ydata = [];
     var ur, title;
-    if (type == 1) { $("#gridr").empty() }
-    else if (type == 2) { $("#gridm").empty() }
-    else { $("#gridp").empty() }
+    if (type == 1) { $("#gridr").empty() } else if (type == 2) { $("#gridm").empty() } else { $("#gridp").empty() }
     for (var i = 0; i < data.length; i++) {
 
         if (type == 1) {
@@ -144,23 +141,21 @@ function gridr(data, type) {
 
             //tjson.data.push(data[i].TIMES * 100);
             ur = '#gridr';
-            title = "次品率分布图--（原因）";
-        }
-        else if (type == 2) {
+            title = lang.Order.Title_1;
+        } else if (type == 2) {
 
             categories.push(data[i].MAC_NO);
             //tjson.name = data[i].MAC_NAME;
             //tjson.data.push(data[i].INFERIOR_NUM_RATE * 100);
             ur = "#gridm";
-            title = "次品率分布图--（设备）";
-        }
-        else {
+            title = lang.Order.Title_2;
+        } else {
 
             categories.push(data[i].MEN_NO);
             //tjson.name = data[i].MEN_NO;
             //tjson.data.push(data[i].INFERIOR_NUM_RATE * 100);
             ur = "#gridp";
-            title = "次品率分布图--（人员）";
+            title = lang.Order.Title_3;
         }
 
 
@@ -173,25 +168,23 @@ function gridr(data, type) {
     for (var j = 0; j < categories.length; j++) {
         var datatemp = [];
         if (type == 1) {
-            tjson.name = "原因-次品个数";
+            tjson.name = lang.Order.FootTitle_1;
             datatemp = _.where(data, { REASON_NAME: categories[j] })
-            if (datatemp.length > 0)
-            {
+            if (datatemp.length > 0) {
                 for (var n = 0; n < datatemp.length; n++) {
                     tjson.data.push(datatemp[n].NUMERATOR);
                 }
             }
         } else if (type == 2) {
-            tjson.name = "设备-次品个数";
+            tjson.name = lang.Order.FootTitle_2;
             datatemp = _.where(data, { MAC_NO: categories[j] })
             if (datatemp.length > 0) {
                 for (var n = 0; n < datatemp.length; n++) {
                     tjson.data.push(datatemp[n].NUMERATOR);
                 }
             }
-        }
-        else {
-            tjson.name = "人员-次品个数";
+        } else {
+            tjson.name = lang.Order.FootTitle_3;
             datatemp = _.where(data, { MEN_NO: categories[j] })
             if (datatemp.length > 0) {
                 for (var n = 0; n < datatemp.length; n++) {
@@ -233,8 +226,7 @@ function drawHisChart(ele, data, title, categories) {
         subtitle: {
             text: ""
         },
-        xAxis:
-        {
+        xAxis: {
             categories: categories,
             labels: {
                 rotation: -45,
@@ -252,7 +244,7 @@ function drawHisChart(ele, data, title, categories) {
             }
         },
         tooltip: {
-            pointFormat: '次品个数：<b>{point.y}</b>',
+            pointFormat: lang.Order.DefectiveNumber + '：<b>{point.y}</b>',
         },
         credits: {
             enabled: false
@@ -265,7 +257,7 @@ function drawHisChart(ele, data, title, categories) {
                     style: {
                         fontWeight: 'bold'
                     },
-                    formatter: function () {
+                    formatter: function() {
                         return (this.y == null) ? "" : this.y;
                     }
                 }
@@ -277,7 +269,7 @@ function drawHisChart(ele, data, title, categories) {
                     style: {
                         fontWeight: 'bold'
                     },
-                    formatter: function () {
+                    formatter: function() {
                         return (this.y == null) ? "" : this.y;
                     }
                 }
