44e84f3f1ae193536c2826fbf9e1f39272506cb9
ta0ist <hueijin1987@qq.com>
2017-01-23 11:24:08 +0800

    commit

--- Module/MachineParameters/web/controller/diagnosissController.js
+++ Module/MachineParameters/web/controller/diagnosissController.js
@@ -1,16 +1,16 @@
-app.controller('appCtrl', ['$scope', function ($scope) {
+app.controller('appCtrl', ['$scope', function($scope) {
     var baseUrl = "/diagnosis/";
     var TIME;
     $scope.items = [];
-    GetGrouplist(0, "machine/GetGrouplist", $("#treeview-template").html(), "icon-group", function (data) {
+    GetGrouplist(0, "machine/GetGrouplist", $("#treeview-template").html(), "icon-group", function(data) {
         var groupId = parseInt($(data).find('[attr="treenode"]').attr("nodeid"));
-        $.post(baseUrl + "GetMachinesByGourpId", { groupId: groupId }, function (data) {
+        $.post(baseUrl + "GetMachinesByGourpId", { groupId: groupId }, function(data) {
             if (data.Status == 0) {
                 $scope.items = data.Data;
                 if ($scope.items.length > 0) {
                     for (var i = 0; i < $scope.items.length; i++) {
                         $scope.items[i].color = "#CCCCCC";
-                        $scope.items[i].photo = "/" + $scope.items[i].PHOTO;
+                        $scope.items[i].photo = "/images/machine/NoDefault/" + $scope.items[i].PHOTO;
                         $scope.items[i].pars = [];
                     }
                 }
@@ -25,18 +25,18 @@ app.controller('appCtrl', ['$scope', function ($scope) {
             }
         });
     });
-    $scope.showDetail = function (index) {
-        location.href =  "/diagnosisdetail?no=" + $scope.items[index].MAC_NBR + "&type=" + $scope.items[index].CATEGORY;
+    $scope.showDetail = function(index) {
+        location.href = "/diagnosisdetail?no=" + $scope.items[index].MAC_NBR + "&type=" + $scope.items[index].CATEGORY;
     }
-    $scope.getRealData = function () {
+    $scope.getRealData = function() {
         var machineIds = "";
         for (var i = 0; i < $scope.items.length; i++) {
             machineIds += $scope.items[i].MAC_NBR + ',';
         }
         machineIds = machineIds.substring(0, machineIds.length - 1);
-        $.post("/diagnosis/GetImmediatelyparameter", ({ machineIds: machineIds }), function (data) {
+        $.post("/diagnosis/GetImmediatelyparameter", ({ machineIds: machineIds }), function(data) {
             for (var i = 0; i < $scope.items.length; i++) {
-                $.each(data.Data.MAC_DATA, function (a, b) {
+                $.each(data.Data.MAC_DATA, function(a, b) {
                     if ($scope.items[i].MAC_NBR == a) {
                         $scope.items[i].pars = [];
                         for (var k = 0; k < b.DATAITEMS.length; k++) {
@@ -49,24 +49,21 @@ app.controller('appCtrl', ['$scope', function ($scope) {
                                         UNIT: b.DATAITEMS[k].UnitName
                                     }
                                     $scope.items[i].color = b.DATAITEMS[k].Value == 0 ? "#CCCCCC" : data.Data.STATUS_DATA[b.DATAITEMS[k].Value].COLOR16;
-                                }
-                                else if (b.DATAITEMS[k].Name == "STD::SubStatus") {
+                                } else if (b.DATAITEMS[k].Name == "STD::SubStatus") {
                                     tjson = {
                                         NAME: b.DATAITEMS[k].Description,
                                         VALUE: b.DATAITEMS[k].Value == 0 ? "-- -- " : data.Data.STATUS_DATA[b.DATAITEMS[k].Value].NAME,
                                         UNIT: b.DATAITEMS[k].UnitName
                                     }
 
-                                }
-                                else if (b.DATAITEMS[k].Name == "STD::StartTime" || b.DATAITEMS[k].Name == "DeviceDateTime") {
+                                } else if (b.DATAITEMS[k].Name == "STD::StartTime" || b.DATAITEMS[k].Name == "DeviceDateTime") {
                                     tjson = {
                                         NAME: b.DATAITEMS[k].Description,
 
                                         VALUE: moment(b.DATAITEMS[k].Value).format('YYYY-MM-DD hh:ss:mm'),
                                         UNIT: b.DATAITEMS[k].UnitName
                                     }
-                                }
-                                else {
+                                } else {
                                     tjson = {
                                         NAME: b.DATAITEMS[k].Description,
                                         VALUE: b.DATAITEMS[k].Value,
@@ -80,7 +77,7 @@ app.controller('appCtrl', ['$scope', function ($scope) {
                     }
                 })
             }
-            TIME = setTimeout(function () {
+            TIME = setTimeout(function() {
                 $scope.getRealData();
             }, 5000);
         });
