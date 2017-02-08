f434256caa7f34ce328c8839f2a7bfd788304fed
ta0ist <hueijin1987@qq.com>
2017-02-08 17:35:31 +0800

    commit

--- Module/MachineParameters/node/routes/diagnosiss.js
+++ Module/MachineParameters/node/routes/diagnosiss.js
@@ -1,10 +1,26 @@
 var diagnosisctrl = require('../controller/Diagnosis.js');
 
-module.exports = function (app) {
+module.exports = function(app) {
     //加载设备管理页面
-     app.get('/diagnosis', diagnosisctrl.diagnosispage);
-      app.get('/diagnosisdetail', diagnosisctrl.diagnosisdetailpage);
-     //app.post('/GetImmediatelyparameter', diagnosisctrl.GetImmediatelyparameter);
-     app.post('/diagnosis/:method', diagnosisctrl.fun);
+    app.get('/diagnosis', diagnosisctrl.diagnosispage);
+    app.get('/diagnosisdetail', diagnosisctrl.diagnosisdetailpage);
+    //app.post('/GetImmediatelyparameter', diagnosisctrl.GetImmediatelyparameter);
+    app.post('/diagnosis/:method', diagnosisctrl.fun);
+
+    app.post('file/:filename', function(req, res, next) {
+        var fileName = req.params.fileName;
+        var filePath = path.join(__dirname, fileName);
+        var stats = fs.statSync(filePath);
+        if (stats.isFile()) {
+            res.set({
+                'Content-Type': 'application/octet-stream',
+                'Content-Disposition': 'attachment; filename=' + fileName,
+                'Content-Length': stats.size
+            });
+            fs.createReadStream(filePath).pipe(res);
+        } else {
+            res.end(404);
+        }
+    });
 
 }
