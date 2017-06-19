777078caeecc376ed5572a9a544273706c9b63f5
ta0ist <hueijin1987@qq.com>
2017-06-19 14:22:05 +0800

    commit

--- Module/MachineParameters/node/controller/HeatTreament.js
+++ Module/MachineParameters/node/controller/HeatTreament.js
@@ -1,11 +1,39 @@
-var path = require('path');
-var request = require('request');
+var path = require('path')
+var request = require('request')
 var config = require('../../../../routes/config.js')
-var post_argu = require('../../../../routes/post_argu.js');
+var post_argu = require('../../../../routes/post_argu.js')
+var db = require('../../../../routes/db')
 
 exports.index = function(req, res) {
     post_argu.permission(req, res, '/HeatTreament', 'view', path.resolve(__dirname, '../../web/view/HeatTreament/index'));
 }
 exports.dayin = function(req, res) {
     res.render(path.resolve(__dirname, '../../web/view/HeatTreamentPrint/index'));
+}
+
+exports.fun = function(req, res) {
+    var args = [];
+    args.push(res);
+    args.push(req.body);
+    doCallback(eval(req.params.method), args, res);
+}
+
+function doCallback(fn, args, res) {
+    fn.apply(this, args);
+}
+
+//获取热处理炉
+function GetMachinesByGourpId(res, args) {
+    db.sql('select * from MACHINE_GROUP_INFO where pid = 23', function(err, result) {
+        if (err) {
+            res.json({
+                Data: err,
+                Status: -9999
+            })
+        }
+        res.json({
+            Data: result,
+            Status: 0
+        })
+    })
 }
--- Module/MachineParameters/node/routes/HeatTreament.js
+++ Module/MachineParameters/node/routes/HeatTreament.js
@@ -4,4 +4,6 @@ module.exports = function(app) {
     //加载设备管理页面
     app.get('/HeatTreament', HeatTreamentctrl.index);
     app.get('/HeatTreamentPrint', HeatTreamentctrl.dayin);
+
+    app.post('/HeatTreatment/:method', HeatTreamentctrl.fun);
 }
